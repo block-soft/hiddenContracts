163f6db0af6047316e423569df492d0977c9c47f
ksu <ksu.zhytomirsky@gmail.com>
2018-04-12 02:19:14 +0300

    init

---
+++ app/app.js
@@ -0,0 +1,86 @@
+var App = {
+    version: 'soljson-v0.4.21+commit.dfe3193c.js', //solc compiler version
+    solc: false, //global compiler object for solidity, preloaded
+    account: false, //current metamask account
+    network: '', //current metamask network
+
+    init: function () {
+        App._getCompiler();
+        App._getNetwork();
+    },
+
+    compile : function (title, code) {
+        UiAlerts.addSuccess(title, 'compiling...');
+        var result_main = App.solc.compile(code, true);
+        if (result_main.errors) {
+            $.each(result_main.errors, function (index, error) {
+                UiAlerts.addError(title, error);
+            });
+            return false;
+        }
+        UiAlerts.addSuccess(title, 'compiled');
+        return result_main.contracts[':' + title];
+    },
+
+    linkTX : function (tx) {
+        return '<a href="' + App.network + '/tx/' + tx + '" target="_blank">' + tx + '</a>';
+    },
+
+    linkAddr : function (addr) {
+        return '<a href="' + App.network + '/address/' + addr + '" target="_blank">' + addr + '</a>';
+    },
+
+    _finished : 0,
+
+    _getCompiler: function () {
+        BrowserSolc.loadVersion(App.version, function (compiler) {
+            UiAlerts.addSuccess('solc', 'loaded');
+            App.solc = compiler;
+            App._setInitFinished();
+        });
+    },
+
+    _getNetwork: function () {
+        web3.version.getNetwork(function (error, data) {
+            switch (data) {
+                case "1":
+                    App.network = 'https://etherscan.io';
+                    break
+                case "3":
+                    App.network = 'https://ropsten.etherscan.io';
+                    break;
+                case "4":
+                    App.network = 'https://rinkeby.etherscan.io';
+                    break;
+                case "41":
+                    App.network = 'https://kovan.etherscan.io';
+                    break;
+                default:
+                    console.log('This is an unknown network.')
+            }
+
+            web3.eth.getAccounts(function (error, data) {
+                if (error) {
+                    $('#info_accounts').html('<h2>No accounts</h2> ' + error.toString());
+                } else if (!data || data.length == 0) {
+                    $('#info_accounts').html('<h2>No accounts</h2> plz setup metamask accounts');
+                } else {
+                    App.account = data[0];
+                    $('#info_accounts_owner').html('<a class="btn btn-info" href="' + App.network + '/address/' + App.account + '" target="_blank">' + App.account + '</a>');
+                    App._setInitFinished();
+                }
+            });
+        });
+    },
+
+    _setInitFinished: function () {
+        App._finished++;
+        if ( App._finished >= 2) {
+            $('#loading_boxes').hide();
+            $('#loaded_boxes').show();
+            $('#contract_button_0').hide();
+            $('#contract_button_1').show();
+            Contracts._restore();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+
+
+##Example
+
+###Main
+
+https://ropsten.etherscan.io/address/0x647f22069adf3bf00e8ec4ca4c8eb5dfd92783a1
+
+MainContract.pay was changed to default function but all other logic the same
+
+### Matching Hidden
+
+https://ropsten.etherscan.io/address/0xc10c925654ad2b9a0a0e37808f61cdbf7d2fcf4b
+
+### Not Mathing Hidden
+
+without optimization
+
+https://ropsten.etherscan.io/address/0xbf24f06ca6710e35ec7555d40a5069ebdc016257
+
+with optimization
+
+https://ropsten.etherscan.io/address/0xb44e681a645cbeb0e38c944d138f455fdccd2e67
